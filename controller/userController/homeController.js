c5328dc455995f4829ff3de3c1f30d0d90a1db95
berksara145 <berksara145@gmail.com>
2023-02-04 23:16:36 +0300

    home
    home da -5 + 15

--- controller/userController/homeController.js
+++ controller/userController/homeController.js
@@ -1,22 +1,58 @@
 const { default: mongoose } = require("mongoose");
+const express = require("express");
 const surahDB = require("../../models/surah");
 const dotenv = require("dotenv");
 dotenv.config();
 
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
 module.exports.home = async (req, res) => {
   try {
-    const fatiha = await surahDB.find({
-      surah_no: 1,
-    });
-    const surahs = await surahDB
-      .find({}, { surah_no: 1, _id: 1, name: 1 })
-      .limit(20);
+    let lastCompleted = req.body.param;
+    let start;
+    let end;
+
+    //setting the range query
+    if (lastCompleted < 6) {
+      // first 20
+      start = 0;
+      end = 20;
+    } else if (lastCompleted < 101) {
+      // -5 +15 of lastCompleted
+      start = lastCompleted - 5;
+      end = lastCompleted + 14;
+    } else {
+      // last 20
+      start = 95;
+      end = 114;
+    }
+
+    //getting nextSurah as + 1
+    let nextSurah;
+    if (114 == lastCompleted) {
+      nextSurah = await surahDB.find({
+        surah_no: 1,
+      });
+    } else {
+      nextSurah = await surahDB.find({
+        surah_no: lastCompleted + 1,
+      });
+    }
+
+    const surahs = await surahDB.find(
+      { surah_no: { $gte: start, $lte: end } },
+      { surah_no: 1, _id: 1, name: 1 }
+    );
 
     res.status(200).json({
-      detailedSurah: fatiha[0],
+      detailedSurah: nextSurah,
       surahList: surahs,
     });
   } catch (err) {
+    console.log(err);
     res.status(500).json({
       error: err,
       message: "server error",
