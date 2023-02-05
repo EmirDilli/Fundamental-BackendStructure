5d4c460a05e1c6195b971b4b27bbe09c0c009b47
Serhat Merak <42006836+merakserhat@users.noreply.github.com>
2023-02-05 14:19:04 +0300

    feat: do not verify token if user not exists

--- controller/authController/veryTokenHandler.js
+++ controller/authController/veryTokenHandler.js
@@ -16,6 +16,10 @@ module.exports.verifyToken = async (req, res) => {
     );
     console.log("got user: ", user);
 
+    if (!user) {
+      return res.status(404).json({ message: "user dos not exists" });
+    }
+
     return res.status(200).json({ message: "success", user });
   } catch (error) {
     return res.status(401).json({ message: "error", error });
