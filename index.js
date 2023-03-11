2130bcaffc236d5115c0db7b275329012096e1f2
Serhat Merak <42006836+merakserhat@users.noreply.github.com>
2023-03-11 23:52:02 +0300

    fix: enable cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = 3001;
 const mongoose = require("mongoose");
 
-var cors = require("cors");
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
