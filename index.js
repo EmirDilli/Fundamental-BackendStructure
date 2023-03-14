f895546deda3240b87110d9ca4357de32bf1a90e
Serhat Merak <42006836+merakserhat@users.noreply.github.com>
2023-03-15 01:37:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = 3001;
 const mongoose = require("mongoose");
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded());
 
 env.config();
